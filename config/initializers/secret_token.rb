441233a7b9e1bd35a3eccbc7317e1a35b4763884
yt <yt@ubuntu.(none)>
2013-09-13 13:17:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebBlog::Application.config.secret_token = '0cd5bdd3557cd8152608e188fc21243e5518bf2a6e0462ae4e54945dce21004a7c48915d519cad8f3564436652185686d5bcc068a56b9a163cefef70488ad700'
