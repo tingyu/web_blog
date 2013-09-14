f7bad4b3dc77db688b759ba276a6bea984f4f6f2
tingyu <ytcleartime@gmail.com>
2013-09-14 04:13:31 -0700

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap-will_paginate', '0.0.6'
 group :development, :test do
 gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.11.0'
+gem 'annotate', '2.5.0'
 end
 # Gems used only for assets and not required
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Post do
